7aeccaec89c937ba8c9e4abb7cddb08f4efcc866
tiger <tigernightshadow@gmail.com>
2025-06-01 22:49:51 +0300

    Initial commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.example.courseworkp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.zaxxer.hikari;
+    requires java.sql;
 
 
     opens org.example.courseworkp to javafx.fxml;
